e905b847b0c3681e0c7cf37dd64b9f1b5a51905c
damilola <oyeyipo45@gmail.com>
2022-03-23 10:37:25 +0100

    configure store

--- src/model/contactModel.ts
+++ src/model/contactModel.ts
@@ -1,4 +1,4 @@
-export interface contact {
+export interface Contact {
     id?: string;
     name?: string
     email?: string
--- src/services/contactsApi.tsx
+++ src/services/contactsApi.tsx
@@ -1,1 +1,16 @@
-import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { Contact} from "../model/contactModel"
+
+
+export const contactsApi = createApi({
+    reducerPath: "contactApi",
+    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+    endpoints: (builder) => ({
+        contacts: builder.query<Contact[], void>({
+            query: () => '/contacts'
+        })
+    })
+})
+
+
+export const { useContactsQuery } = contactsApi
